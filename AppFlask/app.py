9a7d3c23f4080dff78b93de8b9c3c6b7a5ed9caf
ilcapone <juanzumbido@hotmail.com>
2019-09-03 12:07:28 +0200

    basic app

---
+++ AppFlask/app.py
@@ -0,0 +1,18 @@
+from flask import Flask, request
+import os
+app = Flask(__name__)
+import logging
+logging.basicConfig(level=logging.DEBUG)
+
+@app.route('/')
+def hello_world():
+    return 'Vulnerable App'
+
+@app.route('/ls/<path:filename>')
+def ls(filename):
+    output="</br>".join(os.popen('ls ' + filename).readlines())
+    return """
+    <html><body>""" + output + """</body></html>
+    """
+if __name__ == '__main__':
+    app.run(debug=True,host='0.0.0.0'
--- Dockerfile
+++
@@ -1,3 +0,0 @@
-FROM alpine:latest
-RUN apk add nmap tcpdump curl netcat-openbsd nikto --no-cache && rm -f /var/cache/apk/*
-ENTRYPOINT ["./bin/sh"]
